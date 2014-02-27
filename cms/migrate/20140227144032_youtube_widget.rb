22169d910c02086825660ee227b252761952bb26
Rouven Behnke <rouven.behnke@infopark.de>
2014-02-27 16:29:44 +0100

    Youtube Widget

---
+++ cms/migrate/20140227144032_youtube_widget.rb
@@ -0,0 +1,28 @@
+class YoutubeWidget < ::RailsConnector::Migration
+  def up
+    create_obj_class(
+      name: 'YoutubeWidget',
+      type: 'publication',
+      title: 'Youtube',
+      attributes: [
+        {
+          name: 'source',
+          type: :linklist,
+          max_size: 1,
+        },
+        {
+          name: 'max_width',
+          type: :string,
+        },
+        {
+          name: 'max_height',
+          type: :string,
+        },
+      ],
+      preset_attributes: {
+        max_width: '660',
+        max_height: '430',
+      },
+    )
+  end
+end
