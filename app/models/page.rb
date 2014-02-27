799d9f968f99d02599b4051e2691857fdbd5e967
Rouven Behnke <rouven.behnke@infopark.de>
2014-02-27 17:25:27 +0100

    Search-Fix && Breadcrump

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,17 @@
 # This base class provides behavior that all CMS pages have in common. It is
 # similar to a +Widget+, as it allows to add behavior by inheritance.
 class Page < Obj
+  # By default pages display a breadcrumb navigation. Either add a
+  # boolean cms attribute +show_breadcrumbs+ or override the method directly
+  # in your page model.
+  def show_breadcrumbs?
+    true
+  end
+
+  # Returns all breadcrumb pages. A breadcrumb page must be a +Page+ and needs
+  # to allow to be displayed in the navigation. Both +Root+ and +Website+ are
+  # not pages, so only pages up to the homepage are displayed.
+
   # By default, objects can be displayed in navigation sections. Either add a
   # boolean cms attribute +show_in_navigation+ or override the method directly
   # in your model.
