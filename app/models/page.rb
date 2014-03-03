db7100d9330b435702ed83bf1b8495add886a604
Rouven Behnke <rouven.behnke@infopark.de>
2014-03-03 10:05:24 +0100

    Fix Search

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,6 +11,10 @@ class Page < Obj
   # Returns all breadcrumb pages. A breadcrumb page must be a +Page+ and needs
   # to allow to be displayed in the navigation. Both +Root+ and +Website+ are
   # not pages, so only pages up to the homepage are displayed.
+  def breadcrumbs
+    list = ancestors.select { |obj| obj.respond_to?(:show_in_navigation?) && obj.show_in_navigation? }
+    list + [self]
+  end
 
   # By default, objects can be displayed in navigation sections. Either add a
   # boolean cms attribute +show_in_navigation+ or override the method directly
