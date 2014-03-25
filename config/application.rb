773bffe3631a3f8793fee2c0b3152a0709216696
Rouven Behnke <rouven.behnke@infopark.de>
2014-03-25 14:44:34 +0100

    Scrival-SDK Configs

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Cc71
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
+    # Enable the asset pscrivaleline
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
--- config/initializers/rails_connector.rb
+++ config/initializers/rails_connector.rb
@@ -1,26 +1,26 @@
 def content_service_config
-  config = YAML.load_file(Rails.root + 'config/rails_connector.yml')
+  config = YAML.load_file(Rails.root + 'config/scrival.yml')
   config['content_service'] || {}
 rescue Errno::ENOENT
   {}
 end
 
 def cms_config
-  config = YAML.load_file(Rails.root + 'config/rails_connector.yml')
+  config = YAML.load_file(Rails.root + 'config/scrival.yml')
   config['cms_api'] || {}
 rescue Errno::ENOENT
   {}
 end
 
-RailsConnector::Configuration.content_service_url = ENV['CONTENT_SERVICE_URL'] || content_service_config['url']
-RailsConnector::Configuration.content_service_login = ENV['CONTENT_SERVICE_LOGIN'] || content_service_config['login']
-RailsConnector::Configuration.content_service_api_key = ENV['CONTENT_SERVICE_API_KEY'] || content_service_config['api_key']
+Scrival::Configuration.content_service_url = ENV['CONTENT_SERVICE_URL'] || content_service_config['url']
+Scrival::Configuration.content_service_login = ENV['CONTENT_SERVICE_LOGIN'] || content_service_config['login']
+Scrival::Configuration.content_service_api_key = ENV['CONTENT_SERVICE_API_KEY'] || content_service_config['api_key']
 
-RailsConnector::Configuration.cms_url = ENV['CMS_URL'] || cms_config['url']
-RailsConnector::Configuration.cms_login = ENV['CMS_LOGIN'] || cms_config['login']
-RailsConnector::Configuration.cms_api_key = ENV['CMS_API_KEY'] || cms_config['api_key']
+Scrival::Configuration.cms_url = ENV['CMS_URL'] || cms_config['url']
+Scrival::Configuration.cms_login = ENV['CMS_LOGIN'] || cms_config['login']
+Scrival::Configuration.cms_api_key = ENV['CMS_API_KEY'] || cms_config['api_key']
 
-RailsConnector::Configuration.choose_homepage do |env|
+Scrival::Configuration.choose_homepage do |env|
   # Returns an introduction page, when no Homepage found. Usually, you can delete
   # the NullHomepage.new fallback once you first published your content. See
   # "app/controllers/null_homepage_controller.rb" and
@@ -33,6 +33,6 @@ end
 # It is used to provide inplace editing features. Even if you don't use inplace editing
 # on the client side, the server side also uses this callback to determine if CMS data
 # can be modified in the database.
-RailsConnector::Configuration.editing_auth do |env|
+Scrival::Configuration.editing_auth do |env|
   EditModeDetection.editing_allowed?(env)
 end
